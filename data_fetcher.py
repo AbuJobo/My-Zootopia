413853511cc72ce1300fb9c3a5b037e472768104
AbuJobo <t557tbbnkk@privaterelay.appleid.com>
2026-05-07 10:31:29 +0200

    Finally - .env Created to ensure KEY security the proper way.

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,34 +1,17 @@
 import os
-import sys
+from dotenv import load_dotenv
 import requests
+load_dotenv()
 
 API_URL = "https://api.api-ninjas.com/v1/animals"
-
-
-def load_api_key():
-    file_path = "apikey.txt"
-
-    if os.path.exists(file_path):
-        with open(file_path, "r", encoding="utf-8") as file:
-            api_key = file.read().strip()
-            if api_key:
-                return api_key
-
-    api_key = input("Please enter your API key: ").strip()
-
-    if not api_key:
-        print("No API key provided. Program will exit.")
-        sys.exit(1)
-
-    return api_key
+API_KEY = os.getenv("API_NINJAS_KEY")
 
 
 def fetch_data(animal_name):
-    api_key = load_api_key()
 
     response = requests.get(
         API_URL,
-        headers={"X-Api-Key": api_key},
+        headers={"X-Api-Key": API_KEY},
         params={"name": animal_name},
         timeout=30,
     )
